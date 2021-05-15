3bc25456dc944c85918a9c66e5beb78f3e884846
Risky St <67328019+RiskySt@users.noreply.github.com>
2021-05-15 18:53:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+        const menu = document.querySelector('.hamburger');
+        const navbar = document.querySelector('.nav-Links');
+
+        menu.addEventListener('click', function(){
+            navbar.classList.toggle('change');
+        })
